cbe6728dfd2e944fd35eca00bdf5d900a8e6a3ed
“michaelmcqueen” <michael@variantstudios.com>
2016-12-31 16:05:32 -0700

    Working on Nav

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
 
   // Menu Sub Links
   $(".has-submenu").on('click touch', function() {
-    $(this).toggleClass('submenu--open');
-    sr.reveal('.has-submenu li', { 
-      delay: 140,
-      duration: 900,
-      origin: 'top',
-      scale: 1,
-    }, 200);
+     $(this).toggleClass('submenu--open');
+    // sr.reveal('.has-submenu li', { 
+    //   delay: 140,
+    //   duration: 900,
+    //   origin: 'top',
+    //   scale: 1,
+    // }, 200);
   });
 
     $('.gallery-slider').slick({
