754194638f59c7c5245fd7fde8a5b52be26de51f
“michaelmcqueen” <michael@variantstudios.com>
2017-01-04 11:42:43 -0700

    Blog lauot

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -17,44 +17,5 @@ $(document).ready(function() {
      $(this).toggleClass('submenu--open');
   });
   
-  // Slick Init for Home Page  
-  $('.gallery-slider').slick({
-      centerMode: true,
-      dots: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          }
-        },
-        {
-          breakpoint: 900,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 320,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    });
 
-  });
+});
---
+++ pages/contact/book-app.md
@@ -0,0 +1,8 @@
+---
+title: Book Appointment
+permalink: "/book-appointment/"
+description: Contact page description.
+layout: page
+---
+
+{% include book-form.html %}
