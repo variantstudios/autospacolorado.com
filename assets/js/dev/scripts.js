859093a5359ef99cbe94b53d4a7ddabeaf3f9271
brentrobbins <brent@variantstudios.com>
2017-02-28 18:54:14 -0700

    Cloudinary js tweaks.

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -56,35 +56,4 @@ $(document).ready(function() {
         $('nav.main-nav .has-submenu').removeClass('submenu--open');
     });
 
-    function isImageOk(img) {
-        // During the onload event, IE correctly identifies any images that
-        // weren't downloaded as not complete. Others should too. Gecko-based
-        // browsers act like NS4 in that they report this incorrectly.
-        if (!img.complete) {
-            return false;
-        }
-
-        // However, they do have two very useful properties: naturalWidth and
-        // naturalHeight. These give the true size of the image. If it failed
-        // to load, either of these should be zero.
-        if (typeof img.naturalWidth != "undefined" && img.naturalWidth == 0) {
-            return false;
-        }
-
-        // No other way of checking: assume it's ok.
-        return true;
-    };
-
-    // Check all of the images and if they have a data-img value and the origonal src image can't load it'll load the image from the data-img value
-    for (var i = 0; i < document.images.length; i++) {
-        if (!isImageOk(document.images[i])) {
-            //document.images[i].style.visibility = "hidden";
-            console.log(document.images[i].attributes["data-img"]);
-            if (document.images[i].attributes["data-img"]) {
-                document.images[i].src = document.images[i].attributes["data-img"].value;
-            }
-
-        }
-    }
-
 });
